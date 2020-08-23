31f742c5f3e5f46ec8dabde60a4bc80641fbcffa
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2020-08-23 15:01:43 -0300

    Updates

---
+++ precache-manifest.e2aa602bf18d3630f0a08002c5b34867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3f1cfbf3bdd3a8041f08653a2455df",
+    "url": "/react-udemy-seasons/index.html"
+  },
+  {
+    "revision": "d2a356fb795746c4afc0",
+    "url": "/react-udemy-seasons/static/css/main.5f0a6c2e.chunk.css"
+  },
+  {
+    "revision": "00b5835d2019c2695d31",
+    "url": "/react-udemy-seasons/static/js/2.4e8ed336.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-udemy-seasons/static/js/2.4e8ed336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a356fb795746c4afc0",
+    "url": "/react-udemy-seasons/static/js/main.b1214d75.chunk.js"
+  },
+  {
+    "revision": "a8fa7a9372914653a47f",
+    "url": "/react-udemy-seasons/static/js/runtime-main.84945bb1.js"
+  }
+]);
