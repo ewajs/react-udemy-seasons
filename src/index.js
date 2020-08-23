b1190c5c4dbf3c1e6d06b6ce1c2b8dbb8c59ece1
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2020-08-23 14:20:12 -0300

    Added loading spinner component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 // Function Based Component
 // const App = () => {
@@ -51,7 +52,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && !this.state.lat) {
-      return <div>Loading...</div>;
+      return <Spinner />;
     }
   }
 }
